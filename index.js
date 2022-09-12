b5021df36379c256713e5946d4d45c34cd57ea19
mkhalil1997 <mkhalil1997@outlook.com>
2022-09-12 23:10:21 +0300

    first few js scripts and HTML and CSS Changes

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("downloadbtn").addEventListener('mouseenter', function(){
+    document.getElementById("download").style.color = "black";
+})
+document.getElementById("downloadbtn").addEventListener('mouseleave', function(){
+    document.getElementById("download").style.color = "white";
+})
