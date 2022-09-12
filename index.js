beca2b0b85c0ba092b53d01ab32ed20b837d160a
mkhalil1997 <mkhalil1997@outlook.com>
2022-09-12 23:53:16 +0300

    updated JS verf

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const email = document.getElementById("email");
 const pnumber = document.getElementById("pnumber");
 const message = document.getElementById("message");
 const submitBtn = document.getElementById("s-button");
+const contactLine = document.getElementById("contact-line");
 let fNameValue, emailAdd, phoneNumber, messageValue;
 
 document.getElementById("downloadbtn").addEventListener('mouseenter', function(){
@@ -29,12 +30,37 @@ message.addEventListener('input', function(event){
 })
 
 submitBtn.addEventListener('click', function(){
-    verification(fNameValue, phoneNumber, phoneNumber, messageValue)
+    verification(fNameValue, phoneNumber, emailAdd, messageValue)
 })
 
 function verification(fn, pn, ea, msg){
-    function pnVerf(pn){
-        if(pn.substring(0,4) != "+961"){ console.log("what") }
+    let eaVerf = ea.split("@")
+    //Check Phone Number
+    if(pn.substring(0,4) != "+961"){ 
+        contactLine.innerHTML = "Phone Number should start with +961";
     }
-    console.log(pn.substring(0,4))
+
+    else if(pn.substring(4,5) == "3"){
+        if(pn.length != 11){
+            contactLine.innerHTML = "Phone Number should be 7 Numbers";
+        }
+    }
+    else if(pn.length != 12){
+        contactLine.innerHTML = "Phone Number should be 8 Numbers";
+    }
+    //Check Name
+    else if(fn.length < 5){
+        contactLine.innerHTML = "First Name should have at least 5 Chars";
+    }
+    //Check Email
+    else if(msg.length < 100){
+        contactLine.innerHTML = "Message should be at least 100 Chars";
+    }
+    //Check Email
+    else if(eaVerf[0].length < 3 || eaVerf[1].length < 5){
+        contactLine.innerHTML = "Your Email Address has an incorrect format";
+    }
+    else{
+        //Make the button work
+    }
 }
